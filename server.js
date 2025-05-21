ce95256be796a2b9ff8806347389fff6b34c5be7
LeoAndres10 <leo1318velasquez@hotmail.es>
2025-05-21 16:47:25 -0600

    Get 3 y 4

--- server.js
+++ server.js
@@ -16,6 +16,16 @@ const usuarios = [{
     "email" : "juan@email.com"
 }];
 
+const categorias = [{
+    "id" : 1,
+    "nombre" : "Electronica"
+}];
+
+const pedidos = [{
+    "id" : 1,
+    "usuario" : "Juan",
+    "total" : 500
+}];
 
 app.get('/api/productos', (req, res) => {
      return res.json(productos);
@@ -23,6 +33,14 @@ app.get('/api/productos', (req, res) => {
 
 app.get('/api/usuarios', (req, res) => {
      return res.json(usuarios);
+});
+
+app.get('/api/categorias', (req, res) => {
+     return res.json(categorias);
+});
+
+app.get('/api/pedidos', (req, res) => {
+     return res.json(pedidos);
 });
 
 app.listen(PORT, () =>{
